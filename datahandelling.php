23a2c0a17f6638d1a7353e9ff5a80b3bf64a5323
Abhinav Upadhyaya <upadhyayaabhinav10@gmail.com>
2025-08-23 14:10:30 +0545

    Update datahandelling.php

--- datahandelling.php
+++ datahandelling.php
@@ -6,22 +6,26 @@
 	 $sex =$_POST['gender'];
 	 $mail =$_POST['email'];
 	 $pw =$_POST['pwd'];
+	 $rpw =$_POST['repwd'];
 	 $date = date('Y-m-d');
 	 $toc =$_POST['toc'] ?? 'Not Agreed';
+	 $hash_pw=password_hash($pw, PASSWORD_DEFAULT);
+	 $hash_rpw=password_hash($rpw, PASSWORD_DEFAULT);
+
+	 
 	 if(strlen($pw)<8){
 	 $disp_pw = str_repeat('*', strlen($pw)); }
 	 else {
 	 $disp_pw = '********';}
 	 
 	 if($toc === 'Not Agreed'){
-	 echo("Not Agreed re <br><br> -_- <br><br>");
+	 echo("Not Agreed to TOC <br><br> -_- <br><br>");
 	 echo("Agree to the TOC<br>");
 	 echo('<a class="back-link" href="javascript:history.back()">Go Back</a>');
 	 }
 	 else { 
 	 if(strlen($pw)<8){
 	 echo("Password must be atlest 8 Characters long  <br><br> -_- <br><br>");
-	 echo("tsk tsk tsk Change it <br>");
 	 echo('<a class="back-link" href="javascript:history.back()">Go Back</a>');
 		 
 	 }else{
@@ -30,8 +34,13 @@
 	 echo("User Gender: " . $sex . "<br>");
 	 echo("User Email: " . $mail . "<br>");
 	 echo("User Password: " . $disp_pw . "<br>");
+	 echo("User Password(hashed): " . $hash_pw . "<br>");
+	 if (password_verify($rpw, $hash_rpw)) {
+		 echo "Login successful!<br>";}
+		else {
+		 echo "Invalid password.<br>";
+}
 	 echo("Joined Date: " . $date . "<br>");
 	 echo("TOC Status: " . $toc . "<br>");}}
  }
-
 ?>
